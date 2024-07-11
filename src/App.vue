58e8b31f730ef9bcb69bcce82fcaae176883404e
petarally <pprenc@unipu.hr>
2024-07-11 10:54:57 +0200

    Dodana registracijska i forma za prijavu

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/login">Prijava</router-link>
+    <router-link to="/login">Prijava</router-link> |
+    <router-link to="/signup">Registracija</router-link>
   </nav>
   <router-view/>
 </template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,93 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="login-container">
+    <div class="left-side" :style="backgroundImageStyle">
+    </div>
+    <div class="right-side">
+      <form class="login-form">
+        <h2>Prijava</h2>
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" id="username" name="username" required>
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email" required>
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password" required>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'SignUpView',
+  computed: {
+    backgroundImageStyle() {
+      // Using require to ensure webpack correctly processes the file path
+      return {
+        backgroundImage: `url(${require('../assets/login.png')})`
+      };
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login-container {
+  display: flex;
+  height: 100vh;
+}
+
+.left-side, .right-side {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.left-side {
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.login-form {
+  max-width: 300px;
+  width: 100%;
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+.form-group label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+.form-group input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+button {
+  width: 100%;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: #007bff;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+</style>
