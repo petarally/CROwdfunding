02fcfaecd3b57c577728a332ff5df45b85cc66f2
petarally <pprenc@unipu.hr>
2024-07-11 10:39:56 +0200

    Dodani bootstrap, login i signup komponente

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,20 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/login',
+    name: 'LoginView',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
+  },
+  {
+    path: '/signup',
+    name: 'SignUpView',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/SignUpView.vue')
   }
 ]
 
