4c44ffa55c6886a1681b0c9ca8439dfe2143b787
petarally <pprenc@unipu.hr>
2024-07-13 12:00:42 +0200

    dodan home page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+<!--
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/login">Prijava</router-link> |
     <router-link to="/signup">Registracija</router-link>
   </nav>
+-->
   <router-view/>
 </template>
 
